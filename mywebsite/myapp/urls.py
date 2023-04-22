e343254e0b21297be75cc11d683e215c5b31740d
thanakornpop <40554151+thanakornpop@users.noreply.github.com>
2023-04-22 14:38:01 +0700

    Delete mywebsite directory

--- mywebsite/myapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import Homepage
-
-urlpatterns = [
-    path('',Homepage),
-]
--- mywebsite/myapp/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def Homepage(request):
-    return HttpResponse('<h1>Hello World</h1>')
